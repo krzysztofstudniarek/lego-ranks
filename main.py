f653614f570c865ec07587372e49e6fa29118c3e
Krzysztof Studniarek <krzysztof.studniarek@coinmetro.com>
2024-09-01 17:53:57 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import configparser
+import urllib.request
+
+app = FastAPI()
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+
+@app.get("/")
+def read_root():
+    if 'REBRICKABLE_API_KEY' not in config['Apis']:
+        return { "status": -1, "message": "Rebrickable API not reachable" }
+    
+    req = urllib.request.Request("https://rebrickable.com/api/v3/lego/sets/")
+    req.add_header("Authorization", f"key {config['Apis']['REBRICKABLE_API_KEY']}")
+
+    return urllib.request.urlopen(req).read()
