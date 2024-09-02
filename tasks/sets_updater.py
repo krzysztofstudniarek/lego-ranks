e438e278ca9c9569a82efc02f58115ba9393a80b
Krzysztof Studniarek <krzysztof.studniarek@coinmetro.com>
2024-09-03 00:49:37 +0200

    minor improvement in script

--- tasks/sets_updater.py
+++ tasks/sets_updater.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 import configparser
+from time import sleep
 import urllib.request
 import motor.motor_asyncio
 
@@ -24,14 +25,16 @@ async def update_sets(url):
                 "$set": {
                     "set_id": set['set_num'],
                     "name": set['name'],
+                    "year": set['year'],
+                    "num_parts": set['num_parts'],
                 }
             },
             upsert=True
         )
 
     document_count = await db.sets.estimated_document_count()
-    print(f"Progress: {document_count/response['count']*100}%")
-
+    print(f"Progress: {document_count/response['count']*100} %")
+    sleep(10)
     if response['next'] is not None:
         await update_sets(response['next'])
     
