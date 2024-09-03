71aa235cd203493978905549ce2e70074152f9f9
Krzysztof Studniarek <krzysztof.studniarek@coinmetro.com>
2024-09-03 11:10:45 +0200

    populate yearly stats

--- api/main.py
+++ api/main.py
@@ -1,23 +1,20 @@
+import bson.json_util as json_util
 from fastapi import FastAPI
 import configparser
-import urllib.request
+import motor.motor_asyncio
 
 app = FastAPI()
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+client = motor.motor_asyncio.AsyncIOMotorClient(config['MongoDB']['URI'])
+db = client.legos
 
 @app.get("/")
 def read_root():
     return { "status": 200}
 
 @app.get("/sets/yearly_stats")
-def read_yearly_stats():
-    return {
-        "status": 200,
-        "data": [
-            {"year": 2010, "sets": 100, "avg_parts": 100},
-            {"year": 2011, "sets": 100, "avg_parts": 100},
-            {"year": 2012, "sets": 100, "avg_parts": 100},
-        ]
-    }
+async def read_yearly_stats():
+    sets_cursor = db.sets_stats.find({}, {"_id": 0})
+    return json_util.dumps(await sets_cursor.to_list(length=100))
